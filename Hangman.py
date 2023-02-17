44beb155144d7dadf408c45e6260be4bc350496f
FlMonkey <75172047+FlMonkey@users.noreply.github.com>
2023-02-17 08:55:07 +0100

    FULLY WORKING VERSION!!!!

--- Hangman.py
+++ Hangman.py
@@ -1,19 +1,87 @@
-<<<<<<< HEAD
 
 # initialise the program
 import turtle
 import random
 import time
-import customtkinter as Ctk
+import tkinter as tk
 
+finalword = []
+input_text = ""
 
-screen = turtle.Screen()
-screen.setup(1000, 500, startx=0, starty=0)
-screen.bgcolor("white")
+root = tk.Tk()
 
+def submit():
+    global input_text
+    input_text = input_box.get()
+    print("User entered:", input_text)
+    input_box.delete(0, 'end')
+    game()# Clear the input box
+    
 
-turtle.speed(0)
-score = turtle.Turtle()
+def setup():
+    global lives
+    global guessword
+    global finalword
+    global guessl
+    global usedlets
+    global hiddenusedlets
+    
+    usedletters.clear()
+    hang.reset()
+    hang.clear()
+    hang.hideturtle()
+    hang.speed(0)
+    hang.pensize(5)
+    hang.pencolor("blue")
+    # draws hanging station
+    hang.penup()
+    hang.backward(200)
+    hang.right(90)
+    hang.forward(100)
+    hang.left(90)
+    hang.pendown()
+    hang.forward(400)
+    hang.backward(200)
+    hang.left(90)
+    hang.forward(300)
+    hang.right(90)
+    hang.forward(150)
+    hang.right(90)
+    hang.forward(30)
+    
+    lives = 6
+    # makes list with word
+    finalword = []
+    guessword = ("")
+    guessl = []
+    for i in makeword():
+        finalword.append(i)
+        guessword = guessword + (i)
+
+    # makes the blank word
+    makeguess(guessword, True, None)
+
+# Create an input box
+input_box = tk.Entry(root)
+input_box.pack()
+
+# Create an enter button
+enter_button = tk.Button(root, text="Enter", command=submit)
+enter_button.pack()
+
+start_button = tk.Button(root, text="Start/Restart", command=setup)
+start_button.pack()
+
+# Create a Turtle window
+canvas = turtle.Canvas(root, width=1000, height=500)
+canvas.pack()
+
+usedletters = turtle.RawTurtle(canvas)
+score = turtle.RawTurtle(canvas)
+hang = turtle.RawTurtle(canvas)
+
+hang.speed(0)
+
 score.penup()
 score.hideturtle()
 score.forward(250)
@@ -21,90 +89,70 @@ guessl = []
 usedlets = ("")
 hiddenusedlets = ("")
 
-usedletters = turtle.Turtle()
+
 usedletters.penup()
 usedletters.hideturtle()
 usedletters.forward(-300)
 
-print("************Welcome to hangman, you have 6 chances to guess the word************")
+root.bind('<Return>', lambda event: submit())
+
 
 with open("words.txt", "r") as file:
     allText = file.read()
     words = list(map(str, allText.split()))
 
-
 def makeword():
     while True:
+        global x
         x = random.choice(words)
-        if len(x) > 3 and len(x) <= 6:
+        if len(x) > 3 and len(x) <= 8:
             print(x)
             return (x)
 
-
-def setup():
-    turtle.hideturtle()
-    turtle.speed(0)
-    turtle.pensize(5)
-    turtle.pencolor("blue")
-    # draws hanging station
-    turtle.penup()
-    turtle.backward(200)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.left(90)
-    turtle.pendown()
-    turtle.forward(400)
-    turtle.backward(200)
-    turtle.left(90)
-    turtle.forward(300)
-    turtle.right(90)
-    turtle.forward(150)
-    turtle.right(90)
-    turtle.forward(30)
-
-
 def drawbody(bpart):
+    global usedlets
+    global hiddenusedlets
     if bpart == 5:  # head
-        turtle.right(90)
-        turtle.circle(20)
-        turtle.left(90)
-        turtle.penup()
-        turtle.forward(40)
-        turtle.pendown()
+        hang.right(90)
+        hang.circle(20)
+        hang.left(90)
+        hang.penup()
+        hang.forward(40)
+        hang.pendown()
     if bpart == 4:  # body
-        turtle.forward(100)
+        hang.forward(100)
     if bpart == 3:  # left leg
-        turtle.left(45)
-        turtle.forward(60)
-        turtle.backward(60)
-        turtle.right(45)
+        hang.left(45)
+        hang.forward(60)
+        hang.backward(60)
+        hang.right(45)
     if bpart == 2:  # right leg
-        turtle.right(45)
-        turtle.forward(60)
-        turtle.backward(60)
-        turtle.left(45)
+        hang.right(45)
+        hang.forward(60)
+        hang.backward(60)
+        hang.left(45)
     if bpart == 1:  # left arm
-        turtle.backward(75)
-        turtle.left(75)
-        turtle.forward(60)
-        turtle.backward(60)
-        turtle.right(75)
+        hang.backward(75)
+        hang.left(75)
+        hang.forward(60)
+        hang.backward(60)
+        hang.right(75)
     if bpart == 0:  # right arm
-        turtle.right(75)
-        turtle.forward(60)
-        turtle.backward(60)
-        time.sleep(1.5)
-        turtle.penup()
-        turtle.home()
-        turtle.backward(55)
-        turtle.right(90)
-        turtle.forward(150)
-        turtle.left(90)
-        turtle.pendown()
-        turtle.pencolor("red")
-        turtle.write("You Lose", font=("Arial", 30, "normal"))
-        time.sleep(1.5)
-
+        hang.right(75)
+        hang.forward(60)
+        hang.backward(60)
+        hang.penup()
+       
+        hang.home()
+        hang.backward(65)
+        hang.right(90)
+        hang.forward(200)
+        hang.left(90)
+        hang.pendown()
+        hang.pencolor("red")
+        hang.write("You Lose \nThe word was: " + x, font=("Arial", 30, "normal"))
+        usedlets = ""
+        hiddenusedlets = ""
 
 # realword needs to be a string, setup is a boolean and guess is a string. This function will make the word with the filled in '-'
 def makeguess(realword, setup, guess):
@@ -126,102 +174,78 @@ def makeguess(realword, setup, guess):
         guessl = newguessl
         score.write(stringconvert(guessl), font=("Verdana", 20, "normal"))
 
-
 def stringconvert(word):
     fword = ""
     for i in word:
         fword += i
     return fword
 
-
-def input_box(prompt, default=None):
-    # create a tkinter window
-    root = Ctk.CTk()
-
-    root.geometry("200x200")
-    root.anchor("center")
-
-    input = Ctk.CTkInputDialog(
-        text=prompt, title=prompt)
-
-    return input.get_input()
-
-    root.mainloop()
-
-
 def game():
     global hiddenusedlets
-    # sets up the game
-    usedletters.clear()
-    turtle.reset()
-    turtle.clearscreen()
-    setup()
-    lives = 6
     global usedlets
+    global lives
+    # sets up the game
+    
 
-    # makes list with word
-    finalword = []
-    guessword = ("")
-    guessl = []
-    for i in makeword():
-        finalword.append(i)
-        guessword = guessword + (i)
-
-    # makes the blank word
-    blank = makeguess(guessword, True, None)
 
     # Asks for input for first letter
-    while finalword:
 
-        pinput = input_box("Enter a letter", "")
 
-        print(pinput)
-        print(finalword)
-        if str(pinput) in str(finalword):
+        
+    pinput = input_text
 
-            for i in range(len(finalword)):
-                if pinput in finalword:
-                    finalword.remove(pinput)
+    print(pinput)
+    print(finalword)
+    
+    if str(pinput) in str(finalword):
 
-            hiddenusedlets = hiddenusedlets + (pinput + ", ")
-            makeguess(guessword, False, pinput)
+        for i in range(len(finalword)):
+            if pinput in finalword:
+                finalword.remove(pinput)
 
-            if not finalword:
-                turtle.penup()
-                turtle.home()
-                turtle.backward(55)
-                turtle.right(90)
-                turtle.forward(150)
-                turtle.left(90)
-                turtle.pendown()
-                turtle.pencolor("green")
-                turtle.write("You win", font=("Arial", 30, "normal"))
-                time.sleep(1.5)
-                game()
+        hiddenusedlets = hiddenusedlets + (pinput + ", ")
+        makeguess(guessword, False, pinput)
+        
 
-        elif pinput in hiddenusedlets:
-            usedletters.clear()
-            usedletters.write("You have already\n used that letter", font=(
-                "Verdana", 20, "normal"))
+        if not finalword:
+            hang.penup()
+            hang.home()
+            hang.backward(55)
+            hang.right(90)
+            hang.forward(150)
+            hang.left(90)
+            hang.pendown()
+            hang.pencolor("green")
+            hang.write("You win", font=("Arial", 30, "normal"))
+            usedlets = ""
+            hiddenusedlets = ""
             time.sleep(1.5)
-            usedletters.clear()
-            usedletters.write(usedlets, font=("Verdana", 20, "normal"))
+            
 
+    elif pinput in hiddenusedlets:
+        usedletters.clear()
+        usedletters.write("You have already\n used that letter", font=(
+            "Verdana", 20, "normal"))
+        time.sleep(1.5)
+        usedletters.clear()
+        usedletters.write(usedlets, font=("Verdana", 20, "normal"))
+
+    else:
+        lives -= 1
+        if lives == 0:
+            drawbody(0)
+            game()
         else:
-            lives -= 1
-            if lives == 0:
-                drawbody(0)
-                game()
-            else:
-                drawbody(lives)
-                usedlets = usedlets + (pinput + ", ")
-                hiddenusedlets = hiddenusedlets + (pinput + ", ")
-                usedletters.write(usedlets, font=("Verdana", 20, "normal"))
-                # draw hangman
+            drawbody(lives)
+            usedlets = usedlets + (pinput + ", ")
+            hiddenusedlets = hiddenusedlets + (pinput + ", ")
+            usedletters.write(usedlets, font=("Verdana", 20, "normal"))
+            
+            # draw hangman
+
+root.mainloop()
 
 
-game()
+
 
 # draw the hangman
-=======
->>>>>>> parent of 73b1ec4 (Update Hangman.py)
